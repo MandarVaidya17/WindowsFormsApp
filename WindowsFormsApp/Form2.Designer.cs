492bfd7636091361756b4a5238c5f4a03d34c22d
MandarVaidya17 <mandarvaidya1711@gmail.com>
2024-04-29 11:26:40 +0530

    Add project files.

---
+++ WindowsFormsApp/Form2.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtEmpId = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtSal = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnShowList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Salary";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(37, 210);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 47);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(157, 210);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 47);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(269, 210);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 47);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(410, 15);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(118, 50);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtEmpId
+            // 
+            this.txtEmpId.Location = new System.Drawing.Point(169, 15);
+            this.txtEmpId.Name = "txtEmpId";
+            this.txtEmpId.Size = new System.Drawing.Size(137, 26);
+            this.txtEmpId.TabIndex = 8;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(169, 50);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(137, 26);
+            this.txtEmpName.TabIndex = 9;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(169, 84);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(137, 26);
+            this.txtCity.TabIndex = 10;
+            // 
+            // txtSal
+            // 
+            this.txtSal.Location = new System.Drawing.Point(169, 118);
+            this.txtSal.Name = "txtSal";
+            this.txtSal.Size = new System.Drawing.Size(137, 26);
+            this.txtSal.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(544, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(756, 277);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // btnShowList
+            // 
+            this.btnShowList.Location = new System.Drawing.Point(610, 325);
+            this.btnShowList.Name = "btnShowList";
+            this.btnShowList.Size = new System.Drawing.Size(258, 36);
+            this.btnShowList.TabIndex = 13;
+            this.btnShowList.Text = "Show all list";
+            this.btnShowList.UseVisualStyleBackColor = true;
+            this.btnShowList.Click += new System.EventHandler(this.btnShowList_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1449, 450);
+            this.Controls.Add(this.btnShowList);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSal);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtEmpId);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtEmpId;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtSal;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnShowList;
+    }
+}
---
+++ WindowsFormsApp/Form2.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form2()
+        {
+            InitializeComponent();
+            string constr = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+            con=new SqlConnection(constr);
+        }
+
+        private void ClearFeilds()
+        {
+            txtEmpId.Clear();
+            txtEmpName.Clear();
+            txtCity.Clear();
+            txtSal.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "insert into employee values(@name,@city,@salary)";
+                cmd=new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name",txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@city",txtCity.Text);
+                cmd.Parameters.AddWithValue("@salary", txtSal.Text);
+                con.Open();
+                int result=cmd.ExecuteNonQuery();
+                if (result >= 1)
+                {
+                    MessageBox.Show("Record INserted");
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ClearFeilds();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "update employee set name=@name,city=@city,salary=@salary where id=@id";
+                cmd=new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@name", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@city", txtCity.Text);
+                cmd.Parameters.AddWithValue("@salary", txtSal.Text);
+                cmd.Parameters.AddWithValue("@id",txtEmpId.Text);
+
+                con.Open();
+                int result=cmd.ExecuteNonQuery();
+                if(result >= 1)
+                {
+                    MessageBox.Show("Record updated");
+                    ClearFeilds();
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { con.Close(); }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from employee where id=@id";
+                cmd=new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id",txtEmpId.Text);
+                con.Open() ;    
+
+                dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        txtEmpName.Text = dr["name"].ToString();
+                        txtCity.Text = dr["city"].ToString();
+                        txtSal.Text = dr["salary"].ToString() ;
+                    }
+                }else
+                {
+                    MessageBox.Show("Record not found");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "delete from employee where id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", txtEmpId.Text);
+                con.Open();
+
+                int result=cmd.ExecuteNonQuery();
+                if (result >= 1)
+                {
+                    MessageBox.Show("Record deleted");
+                    ClearFeilds();
+                }
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnShowList_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from employee";
+                cmd=new SqlCommand(qry, con);
+                con.Open();
+                dr = cmd.ExecuteReader();
+
+                DataTable table = new DataTable();
+                table.Load(dr);
+                dataGridView1.DataSource = table;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
